ff8410741dd471642d80263ce66a49be570ba849
you_mi <imym1002@gmail.com>
2025-04-25 16:58:00 +0900

    변수

--- 250425/first.py
+++ 250425/first.py
@@ -7,12 +7,20 @@
 
 
 
-print("김치볶음밥은 마시썽")
-print("김아무개 와우와우 김치볶음밥")
 
 
 
+name_1 = "도토리묵"
+name_2 = "오징어볶음"
+name_3 = "삼각김밥"
+print(name_1, name_2, name_3)
 
+s= 10
+d= 50
+f= 60
+print(s,"+",d,"=", f)
 
+# 타입 체크
 
-
+print(type(PI))
+print(type(name))
